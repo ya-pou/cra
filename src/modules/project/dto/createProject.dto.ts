4043866ae8c348126ab40180dbf924083b8176b0
WebMate Services <wms@macalex.local>
2025-11-07 13:33:09 +0100

    feat(project): add validation class to createDto

--- src/modules/project/dto/createProject.dto.ts
+++ src/modules/project/dto/createProject.dto.ts
@@ -1,4 +1,13 @@
+import { IsOptional, IsString, Length } from "class-validator";
+
 export class CreateProjectDto {
+  
+  @IsString()
+  @Length(3, 100)
   name!: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(0, 500)
   description?: string;
 }
