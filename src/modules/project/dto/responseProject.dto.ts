2588d756a59fef346bdbfa975d7fe8f90e902ac7
WebMate Services <haag.a1994@gmail.com>
2025-11-07 13:27:48 +0100

    feat(project):add Create and Response DTO

---
+++ src/modules/project/dto/responseProject.dto.ts
@@ -0,0 +1,10 @@
+import { Task } from "../../task/task.entity.js";
+import { User } from "../../user/user.entity.js";
+
+export class ResponseProjectDto {
+  id!: number;
+  name!: string; 
+  description!: string;
+  client!: User;
+  tasks!: Task[]
+}
